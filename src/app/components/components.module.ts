8e5e8210f53676fd75c49a6145a556a17d505892
sekab <amrtaher1995@gmail.com>
2019-01-09 08:09:44 +0200

    added loader component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material.module';
 import { VideoCardComponent } from './video-card/video-card.component';
 import { LoaderComponent } from './loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     declarations : [VideoCardComponent, LoaderComponent] ,
-    imports : [MaterialModule] ,
-    exports : [VideoCardComponent],
+    imports : [MaterialModule , BrowserModule , CommonModule] ,
+    exports : [VideoCardComponent , LoaderComponent],
 })
 export class ComponentsModule { }
--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  styleUrls: ['./loader.component.scss'],
+  animations : [ trigger('basicOpacity', [
+    transition(':enter', [
+      style({ opacity: 0 }),
+      animate('0.65s ease-in', keyframes([
+        style({ opacity: 0, offset: 0 }),
+        style({ opacity: 0.3, offset: 0.3 }),
+        style({ opacity: 0.6, offset: 0.6 }),
+        style({ opacity: 1, offset: 1 }),
+      ]))
+    ]),
+    transition(':leave', [
+      style({ opacity: 1 }),
+      animate('0.5s ease-in', style({ opacity: 0 }))
+    ]),
+  ])
+]
 })
 export class LoaderComponent implements OnInit {
 
+  @Input() Loading: Boolean;
+  @Input() Image: string;
   constructor() { }
 
   ngOnInit() {
